421778e21fe291b7a24f7bb045536eb8174e810a
ImranRH321 <imranrh247@gmail.com>
2022-06-16 04:01:27 +0600

    cereate post - data

--- index.js
+++ index.js
@@ -1,35 +1,47 @@
-const express = require('express');
-const app = express()
-const port = process.env.PORT || 5000
+const express = require("express");
+const cors = require('cors')
+const app = express();
+
+const port = process.env.PORT || 5000;
+app.use(cors())
+app.use(express.json())
 
 // user: dbuser1
 // password: XOU3qtUlwNnL4htR
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://dbuser1:XOU3qtUlwNnL4htR@cluster0.fe8tu.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://dbuser1:XOU3qtUlwNnL4htR@cluster0.fe8tu.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// ...
-async function run () {
-  try{
-     await client.connect()
-     const userCollection  = client.db('foodExpress').collection('users')
-    const user = {name: 'parboti banu', email: 'parboti@gmail.com'}
-    const result = await userCollection.insertOne(user)
-    
-    console.log(`document insert the id ${result.insertedId}`);
-  } 
-  finally{
-    // await client.close(); 
+//  ....
+async function run() {
+  try {
+    await client.connect();
+    const userCollection = client.db("foodExpress").collection("users");
+
+    //  ..
+    app.post("/user", (req, res) => {
+     const newUser = (req.body);
+    //  console.log('create user ', newUser);
+      res.send({result: 'success'})
+    });
+    //  ..
+  } finally {
+    // await client.connect()
   }
 }
-run().catch(console.dir)
-// ...
+run().catch(console.dir);
+//  ....
 
-app.get('/', (req, res) => {
-    res.send('my crud is running ');
-})
+app.get("/", (req, res) => {
+  res.send("my crud is running ");
+});
 
 app.listen(port, () => {
-    console.log('crud is running on port', port);
-})
+  console.log("crud is running on port", port);
+});
